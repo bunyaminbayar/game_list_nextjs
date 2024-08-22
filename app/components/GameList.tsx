765fdaa0bae43aa3416aa1379c007422d0908aa8
bunyaminbayar <bunyaminmore@gmail.com>
2024-08-22 16:37:08 +0200

    replace mock data path

--- app/components/GameList.tsx
+++ app/components/GameList.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useMemo, useEffect } from 'react';
-import Data from '@/app/data/mock.json';
+import Data from '../data/mock.json'
 import GameItems from './GameItems';
 
 
